b74c1f1b51ef7bd7d9c9e3eef584b1b87ee27f92
Nouelle <ncprs10@gmail.com>
2023-06-25 02:08:38 +0800

    Fixed naming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <div className="w-full h-[1000px] bg-page-bg">
