c2c7ee0240efe52581296481101b5b9079216f49
Nouelle Caparas <103345977+nouelledev@users.noreply.github.com>
2023-07-16 15:30:26 +0800

    Revert "Feat: Framer motion"
    This reverts commit bead34dffb53415c409ba603b18ebae07796f722.

--- app/components/Portfolio.tsx
+++ app/components/Portfolio.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
 import React from "react";
-import Reveal from "./Reveal";
 
 const Portfolio = () => {
   const PortfolioProjects = [
@@ -51,45 +50,43 @@ const Portfolio = () => {
     setCurrentIndex(slideIndex);
   };
   return (
-    <Reveal>
-      <div id="portfolio" className="max-w-[1100px] mx-auto mt-[15%]">
-        <h1 className="text-white  mt-[20] text-center pt-[30%] md:pt-[10%] font-bold text-base md:text-5xl">
-          My Portfolio
-        </h1>
-        <div className="max-w-[1200px]  h-[550px] md:h-[780px] w-full m-auto py-16 px-4  relative group ">
-          <div
-            style={{
-              backgroundImage: `url(${PortfolioProjects[currentIndex].imgUrl})`,
-            }}
-            className="w-full h-full rounded-2xl bg-center bg-cover duration-500 "
-          >
-            <h1 className="text-white absolute inset-x-0 bottom-20 h-16 text-center font-bold text-4xl md:text-5xl">
-              {" "}
-              {PortfolioProjects[currentIndex].info}
-            </h1>
-          </div>
-          {/*Left arrow*/}
-          <div className="text-white w-fit group-hover:block  absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20  cursor-pointer">
-            <BsChevronCompactLeft onClick={prevSlide} size={30} />
-          </div>
-          {/*Right arrow */}
-          <div className="text-white w-fit group-hover:block   absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 cursor-pointer">
-            <BsChevronCompactRight onClick={nextslide} size={30} />
-          </div>
-          <div className="flex top-4 justify-center py-2">
-            {PortfolioProjects.map((slide, slideIndex) => (
-              <div
-                key={slideIndex}
-                onClick={() => goToSlide(slideIndex)}
-                className="text-2xl cursor-pointer text-white"
-              >
-                <RxDotFilled />
-              </div>
-            ))}
-          </div>
+    <div id="portfolio" className="max-w-[1100px] mx-auto ">
+      <h1 className="text-white  mt-[20] text-center pt-[30%] md:pt-[10%] font-bold text-base md:text-5xl">
+        My Portfolio
+      </h1>
+      <div className="max-w-[1200px]  h-[550px] md:h-[780px] w-full m-auto py-16 px-4  relative group ">
+        <div
+          style={{
+            backgroundImage: `url(${PortfolioProjects[currentIndex].imgUrl})`,
+          }}
+          className="w-full h-full rounded-2xl bg-center bg-cover duration-500 "
+        >
+          <h1 className="text-white absolute inset-x-0 bottom-20 h-16 text-center font-bold text-4xl md:text-5xl">
+            {" "}
+            {PortfolioProjects[currentIndex].info}
+          </h1>
+        </div>
+        {/*Left arrow*/}
+        <div className="text-white w-fit group-hover:block  absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20  cursor-pointer">
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        {/*Right arrow */}
+        <div className="text-white w-fit group-hover:block   absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 cursor-pointer">
+          <BsChevronCompactRight onClick={nextslide} size={30} />
+        </div>
+        <div className="flex top-4 justify-center py-2">
+          {PortfolioProjects.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className="text-2xl cursor-pointer text-white"
+            >
+              <RxDotFilled />
+            </div>
+          ))}
         </div>
       </div>
-    </Reveal>
+    </div>
   );
 };
 
