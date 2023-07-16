5fdfeea3674d83b1950dea5eb7e38afdfa6a5532
Nouelle <ncprs10@gmail.com>
2023-07-16 14:18:54 +0800

    test

--- app/api/email/route.tsx
+++ app/api/email/route.tsx
@@ -1,6 +1,7 @@
 import ContactEmail from "@/app/email/ContactEmail";
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
+
 const resend = new Resend(process.env.EMAIL_KEY);
 
 export async function POST(request: Request) {
